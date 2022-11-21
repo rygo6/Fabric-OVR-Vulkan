c898df5ebfa7555d79dcf25c0e63efc6bab232d4
rygo6 <rygo6@me.com>
2022-11-20 22:22:21 -0800

    start moxaic naming, start moxaic core

---
+++ src/moxaic_core.h
@@ -0,0 +1,53 @@
+#ifndef MOXAIC_CORE_H
+#define MOXAIC_CORE_H
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <stdbool.h>
+
+typedef struct MxcAppState {
+    int screenWidth;
+    int screenHeight;
+
+    bool enableValidationLayers;
+
+    GLFWwindow *pWindow;
+
+    VkInstance instance;
+    VkDebugUtilsMessengerEXT debugMessenger;
+    VkSurfaceKHR surface;
+
+    VkPhysicalDevice physicalDevice;
+    VkDevice device;
+
+    VkQueue queue;
+    uint32_t graphicsQueueFamilyIndex;
+
+    VkSwapchainKHR swapChain;
+    uint32_t swapChainImageCount;
+    VkImage *pSwapChainImages;
+    VkImageView *pSwapChainImageViews;
+    VkFormat swapChainImageFormat;
+    VkExtent2D swapChainExtent;
+
+    VkFramebuffer *pSwapChainFramebuffers;
+
+    VkRenderPass renderPass;
+    VkPipelineLayout pipelineLayout;
+    VkPipeline graphicsPipeline;
+
+    VkCommandPool commandPool;
+    VkCommandBuffer commandBuffer;
+
+    VkSemaphore imageAvailableSemaphore;
+    VkSemaphore renderFinishedSemaphore;
+    VkFence inFlightFence;
+
+} MxcAppState;
+
+void mxcInitWindow(MxcAppState* pState);
+void mxcInitVulkan(MxcAppState* pState);
+void mxcMainLoop(MxcAppState* pState);
+void mxcCleanup(MxcAppState* pState);
+
+#endif //MOXAIC_CORE_H
